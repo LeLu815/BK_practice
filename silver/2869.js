e627aac76faf9035a1d13d3064b2b5c048393826
In Lee <leein9404@naver.com>
2022-09-22 16:09:33 +0900

    Solution BEKJOON 1316 : add comment

--- silver/2869.js
+++ silver/2869.js
@@ -4,6 +4,15 @@
 //     .toString()
 //     .trim()
 //     .split(' ');
+
+/**
+ * 생각없이 while 문을 돌리게 되면 시간초가가 난다
+ * 올라갔다 내려오는데 한번 끝까지 올라가면 내려오지 않으니 총 몇일이 걸리는지 계산을 해볼 때
+ * 올라간 길이 - 내려간 길이 한 값을 총 가야할 길이에서 빼주면 된다.
+ * 여기서 주의할 점은 마지막에 미끄러지지 않는데 위와 같이 그냥 나눠주면 올라갔다가 미끄러지는 것까지 계산된 값이 되므로
+ * 실제로 그렇게 날짜를 계산하고 싶으면 총 길이에서 마지막에 미끄러진다고 가정하여 빼준 값을 가야할 총 길이로 구해주면 된다.
+ */
+
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
 const go = +input[0];
@@ -27,4 +36,4 @@ console.log(Math.ceil((goal-back)/(go-back)));
 //     value -= back;
 // }
 
-console.log(period);
+// console.log(period);
