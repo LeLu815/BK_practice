b837876424d5d958a3547ea8d02ee5fa830cbf6c
In Lee <leein9404@naver.com>
2022-10-18 14:15:41 +0900

    Solution BEAKJOON 9020

--- silver/9020.js
+++ silver/9020.js
@@ -1,22 +1,26 @@
-// // const fs = require("fs");
-// // const input = fs
-// //     .readFileSync("/dev/stdin")
-// //     .toString()
-// //     .trim()
-// //     .split('\n')
-// //     .map((n)=>parseInt(n));
+const fs = require("fs");
+const input = fs
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split('\n')
+    .map((n)=>parseInt(n));
 
-// /**
-//  * 숫자를 받으면 2부터 해당 숫자보다 작은 소수들을 담은 배열을 리턴하는 함수
-//  * 해당 숫자들을 조합하여 합이 되는 두 수를 리턴하는 함수
-//  * 두 수의 차이가 가장 작은 조합을 리턴하는 함수
-//  * 
-//  * 해결책1
-//  * 문제 1 : 숫자를 받으면 해당 수보다 작은 소수를 계산하는 과정을 불필요하게 반복함.
-//  *          범위를 받았기 때문에 한번 계산을 해놓고 사용하면 연산 과정을 줄일 수 있다.
-//  * 
-//  * 문제 2 : 대칭적인 수가 생길 수 있기 때문에 중복체크를 해줄 수 있는 배열보다는 Set 객체를 이용한다.
-//  */
+/**
+ * 숫자를 받으면 2부터 해당 숫자보다 작은 소수들을 담은 배열을 리턴하는 함수
+ * 해당 숫자들을 조합하여 합이 되는 두 수를 리턴하는 함수
+ * 두 수의 차이가 가장 작은 조합을 리턴하는 함수
+ * 
+ * 해결책1
+ * 문제 1 : 숫자를 받으면 해당 수보다 작은 소수를 계산하는 과정을 불필요하게 반복함.
+ *          범위를 받았기 때문에 한번 계산을 해놓고 사용하면 연산 과정을 줄일 수 있다.
+ * 문제 2 : 대칭적인 수가 생길 수 있기 때문에 중복체크를 해줄 수 있는 배열보다는 Set 객체를 이용한다.
+ * 
+ * 합과 차는 한끝 차이이다.
+ * 두 수의 합이 n 이 나온다는 조건은 n 과 한 수 의 차가 다른 한수여도 된다는 것을 의미한다.
+ * 두 수의 차는 후반부로 갈수록 작아진다.
+ * 위의 두 개념을 적용하면 연산과정을 효율적으로 줄일 수 있다.
+ */
 
 
 
@@ -27,10 +31,10 @@
 
 // // const input = data.split('\n').map((n)=>parseInt(n));
 
-const input =[4999];
-for(let i=4; i<=10000; i+=2) {
-    input.push(i);
-}
+// const input =[4999];
+// for(let i=4; i<=10000; i+=2) {
+//     input.push(i);
+// }
 
 
 const isPrime = (n) => {
@@ -72,35 +76,41 @@ const combination = (list, num) => {
         //     }
         //     return n===num;
         // });
-        list.map((n, index)=>{
-            if (list[i]+n === num) {
-                answer.push([list[i], list[index]]);
-                rank.push(list[index]-list[i]);
-            }
-            return ;
-        });
+        // list.map((n, index)=>{
+        //     if (list[i]+n === num) {
+        //         answer.push([list[i], list[index]]);
+        //         rank.push(list[index]-list[i]);
+        //     }
+        //     return ;
+        // });
+        const index = list.indexOf(num - list[i]);
+        if (index !== -1) {
+            answer.push([list[i], list[index]]);
+        }
     }
-    return [answer, rank];
+    return answer;
 }
 
-const mostSmallGap = (list, rank) => {
-    // const temp = [];
-    // for(let i=0; i<list.length; i++) {
-    //     // 오름차순 정렬이기 때문에 뒷수에서 앞수를 뺀다.
-    //     // temp.push(list[i].sort((a, b) => a-b).reduce((a,b)=>b-a));
-    //     temp.push(list[i].reduce((a,b)=>b-a));
-    // }
-    const gap = Math.min(...rank);
-    const idx = rank.indexOf(gap);
-    return list[idx];
-}
+// const mostSmallGap = (list, rank) => {
+//     // const temp = [];
+//     // for(let i=0; i<list.length; i++) {
+//     //     // 오름차순 정렬이기 때문에 뒷수에서 앞수를 뺀다.
+//     //     // temp.push(list[i].sort((a, b) => a-b).reduce((a,b)=>b-a));
+//     //     temp.push(list[i].reduce((a,b)=>b-a));
+//     // }
+//     const gap = Math.min(...rank);
+//     const idx = rank.indexOf(gap);
+//     return list[idx];
+// }
 
 const mainList = primeList(10000);
 
 for (let i=1; i<=input.at(0); i++) {
-    const [combiList, rank] = combination(mainList, input[i]);
-    const finalList = mostSmallGap(combiList, rank);
-    // console.log(input[i], finalList);
-    console.log(input[i],':' ,finalList.join(' '));
+    const combiList = combination(mainList, input[i]);
+    console.log(combiList.pop().join(' '));
+    // console.log(combiList.pop());
+    // const finalList = mostSmallGap(combiList, rank);
+    // // console.log(input[i], finalList);
+    // console.log(input[i],':' ,finalList.join(' '));
 }
 
