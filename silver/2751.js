6a022ce5fdca78497311d434f664e980cb42e5f6
In Lee <leein9404@naver.com>
2022-10-22 03:57:38 +0900

    Solution BEAKJOON 2751

---
+++ silver/2751.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+// const input = ['5','5','4','3','2','1'];
+input.shift()
+const answer = input.sort((a,b)=>a-b);
+console.log(answer.join('\n'));
