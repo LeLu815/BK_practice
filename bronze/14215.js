63cf04b43e914f5bac40d7eaa8af185213b82ffc
LinQuixote <leein9404@naver.com>
2023-11-24 07:18:29 +0900

    231124 24262 풀이

--- bronze/14215.js
+++ bronze/14215.js
@@ -1,7 +1,7 @@
-const input = ["1", "2", "3"];
+// const input = ["1", "2", "3"];
 
-// const fs = require("fs");
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
 let [a, b, c] = input.map((n) => parseInt(n)).sort((a, b) => a - b);
 if (a + b > c) {
