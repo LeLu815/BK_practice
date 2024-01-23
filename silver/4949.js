cc8e5a61b00f70a15a19ec1a4e3358c5badc2b83
LinQuixote <leein9404@naver.com>
2024-01-24 08:00:59 +0900

    240124 4949 풀이

--- silver/4949.js
+++ silver/4949.js
@@ -1,16 +1,16 @@
-const input = [
-  "So when I die (the [first] I will see in (heaven) is a score list).",
-  "[ first in ] ( first out ).",
-  "Half Moon tonight (At least it is better than no Moon at all].",
-  "A rope may form )( a trail in a maze.",
-  "Help( I[m being held prisoner in a fortune cookie factory)].",
-  "([ (([( [ ] ) ( ) (( ))] )) ]).",
-  " .",
-  ".",
-];
+// const input = [
+//   "So when I die (the [first] I will see in (heaven) is a score list).",
+//   "[ first in ] ( first out ).",
+//   "Half Moon tonight (At least it is better than no Moon at all].",
+//   "A rope may form )( a trail in a maze.",
+//   "Help( I[m being held prisoner in a fortune cookie factory)].",
+//   "([ (([( [ ] ) ( ) (( ))] )) ]).",
+//   " .",
+//   ".",
+// ];
 
-// const fs = require("fs");
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const resultList = [];
 const isBalanced = (string) => {
   const stringList = string.split("");
